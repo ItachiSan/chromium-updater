0ab342e4fe1c465adbd251e1216e09af173da19a
cfstras <claus@strasburger.de>
2012-03-05 16:32:06 +0100

    added netbeans project
    Signed-off-by: cfstras <claus@strasburger.de>

---
+++ src/net/chromiumupdater/ChromiumUpdater.java
@@ -0,0 +1,11 @@
+package net.chromiumupdater;
+
+/**
+ *
+ * @author morth, cfstras
+ */
+public class ChromiumUpdater {
+    public static void main(String[] args) {
+        GUI.rungui();
+    }
+}
